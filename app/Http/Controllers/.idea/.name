25972f844f0c4a41371a6ac29fef84a0b0fce075
nabiha17 <nabihamustaqeem@gmail.com>
2022-04-19 15:42:47 +0600

    first commit

---
+++ app/Http/Controllers/.idea/.name
@@ -0,0 +1,1 @@
+OrderController.php
---
+++ resources/views/layouts/front/components/footer.blade.php
@@ -0,0 +1,56 @@
+<div class="pre-footer">
+    <div class="container">
+        <div class="row bottom-border">
+            <div class="col-sm-12 col-md-6 col-lg-3 mt-3">
+                <h1>Amethyst <br> Bookstore</h1>
+            </div>
+
+
+            <div class="col-sm-12 col-md-6 col-lg-3">
+
+                <h2>Important Links</h2>
+
+                <ul class="list-unstyled">
+                    <li><i class="fa fa-angle-right"></i><a href="#">About Us</a></li>
+                    <li><i class="fa fa-angle-right"></i><a href="#">Contact US</a></li>
+                    <li><i class="fa fa-angle-right"></i><a href="#">Guidelines</a></li>
+                    <li><i class="fa fa-angle-right"></i><a href="#">Terms & Services</a></li>
+                    <li><i class="fa fa-angle-right"></i><a href="#">Delivery Information</a></li>
+                    <li><i class="fa fa-angle-right"></i><a href="#">Refund Policy</a></li>
+                </ul>
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-3">
+                <h2>Contact Info</h2>
+                <address class="margin-bottom-40">
+                    Ring Road<br>
+                    Mohammadpur, Dhaka - 1100, Bangladesh <br>
+                    Phone: +880 1792 594 554<br>
+                    Email: <a href="mailto:support@kureghor.com.bd" class="english">support@amethyst.com</a><br>
+                    Skype: <a href="skype:kitabghor" class="english">amethyst</a>
+                </address>
+            </div>
+
+{{--            <div class="col-sm-12 col-md-6 col-lg-3 overflow-hidden">--}}
+{{--                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fshopkureghor&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"--}}
+{{--                        width="340" height="214" style="border:none;overflow:hidden" scrolling="no" frameborder="0"--}}
+{{--                        allowTransparency="true" allow="encrypted-media"></iframe>--}}
+{{--            </div>--}}
+        </div>
+    </div>
+</div>
+
+<!-- pre-footer ends here -->
+
+
+<!-- footer starts here -->
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <p>2022 © Amethyst Bookstore ALL Rights Reserved.</p>
+            </div>
+        </div>
+    </div>
+</div>
